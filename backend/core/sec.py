23b0810bd77bedd80a74f986c5ad08322486c8ba
1velialer-cell <1velialer@gmail.com>
2026-06-06 06:08:20 +0000

    [ref] fix 422 start test

--- backend/core/sec.py
+++ backend/core/sec.py
@@ -1,8 +1,9 @@
-from fastapi import HTTPException
+from fastapi import HTTPException, Header
+from typing import Optional
 from backend.storage import TOKENS
 import time
 
-def verify_token(token: str):
+def verify_token(token: str) -> Optional[str]:
     if not token:
         return None
     info = TOKENS.get(token)
@@ -10,8 +11,10 @@ def verify_token(token: str):
         return None
     return info["user"]
 
-
-def get_current_user(token: str):
+def get_current_user(authorization: Optional[str] = Header(None, alias="Authorization")):
+    if not authorization:
+        raise HTTPException(status_code=401, detail="Token missing")
+    token = authorization.replace("Bearer ", "").strip()
     user = verify_token(token)
     if not user:
         raise HTTPException(status_code=401, detail="invalid token")
